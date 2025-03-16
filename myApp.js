0c097319bcd336dcbcd2d37a491fdcfc5e7b15f4
Anoop <anoopk15076@gmail.com>
2025-03-16 11:39:24 +0000

    Commit message for staged changes

--- myApp.js
+++ myApp.js
@@ -1,39 +1,11 @@
 let express = require('express');
 let app = express();
 
+app.get("/", (req, res) => {
+    // res.send('Hello Express');
+    //  res.sendFile ( __dirname + "/views/index.html");      
+});
 
+app.use('/public',express.static(__dirname+'/public'));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
